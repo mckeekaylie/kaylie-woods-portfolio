cef98e4e5161af827e9982e1296d94c26479a052
mckeekaylie <59926734+mckeekaylie@users.noreply.github.com>
2024-11-10 13:18:24 -0500

    Implement routing animations

--- kaylie-woods-portfolio-app/src/app/app.component.ts
+++ kaylie-woods-portfolio-app/src/app/app.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, FooterComponent],
+  imports: [
+    NavComponent,
+    FooterComponent,
+    RouterModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  animations: [
+    trigger('routeAnimation', [
+      transition('* <=> *', [animate('1s ease-in'), style({ opacity: 1 })]),
+    ]),
+  ],
 })
+
 export class AppComponent {
+  constructor(private router: Router) {}
   title = 'kaylie-woods-portfolio-app';
+
+  getRouteAnimationData() {
+    return this.router.url;
+  }
 }
