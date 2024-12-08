d31607012f448f4f95ec415e4f1f62941dd9ecaf
mckeekaylie <59926734+mckeekaylie@users.noreply.github.com>
2024-12-08 14:54:42 -0500

    Create + implement loading indicator

--- kaylie-woods-portfolio-app/src/app/my-work/my-work.component.ts
+++ kaylie-woods-portfolio-app/src/app/my-work/my-work.component.ts
@@ -1,10 +1,11 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
 import { MyWorkService, myJobs } from '../services/my-work.service';
 import { CommonModule, ViewportScroller } from '@angular/common';
 import { ChallengesProjsComponent } from '../challenges-passion-projs/challenges-projs.component';
 import { HeroComponent } from '../hero/hero.component';
 import { CarouselComponent } from '../carousel/carousel.component';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'my-work',
@@ -13,32 +14,33 @@ import { CarouselComponent } from '../carousel/carousel.component';
     CommonModule,
     ChallengesProjsComponent,
     HeroComponent,
-    CarouselComponent
+    CarouselComponent,
+    LoaderComponent,
   ],
   templateUrl: './my-work.component.html',
   styleUrl: './my-work.component.scss',
 })
-export class MyWorkComponent {
+export class MyWorkComponent implements OnInit, AfterViewInit {
   onDestroy$: Subject<void> = new Subject();
 
-  heroTitle = "My Work"
-  imageUrl = "assets/images/my-work/bkgd-my-work.jpg";
+  loading$: Subject<boolean> = new Subject();
+
+  heroTitle = 'My Work';
+  imageUrl = 'assets/images/my-work/bkgd-my-work.jpg';
 
   proRank = {} as myJobs;
   genesys = {} as myJobs;
   tcc = {} as myJobs;
 
-  @ViewChild('prorank') sectionProrank!: ElementRef;
-  @ViewChild('genesys') sectionGenesys!: ElementRef;
-  @ViewChild('tcc') sectionTcc!: ElementRef;
-  @ViewChild('challenges') sectionChallenges!: ElementRef;
-
-  constructor(private myWorkService: MyWorkService, private viewportScroller: ViewportScroller) {}
+  constructor(
+    private myWorkService: MyWorkService,
+    private viewportScroller: ViewportScroller,
+  ) {}
 
   ngOnInit() {
     this.viewportScroller.scrollToPosition([0, 0]);
     this.viewportScroller.setOffset([0, 100]);
-    
+
     this.myWorkService
       .getMyJobs()
       .pipe(takeUntil(this.onDestroy$))
@@ -56,6 +58,12 @@ export class MyWorkComponent {
           data.find((item) => item.employer === 'TCC Solutions'),
         );
       });
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.loading$.next(false);
+    }, 2500);
   }
 
   scrollToSection(section: string) {
