ed9fe891cb57c5f6468323b50f257e42238b3f4d
mckeekaylie <59926734+mckeekaylie@users.noreply.github.com>
2025-01-15 18:27:33 -0500

    carousel overhaul

--- kaylie-woods-portfolio-app/src/app/shared/components/carousel/carousel.component.ts
+++ kaylie-woods-portfolio-app/src/app/shared/components/carousel/carousel.component.ts
@@ -1,19 +1,6 @@
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
+import { animate, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  HostListener,
-  Input,
-  ViewChild,
-} from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { CarouselImg } from '@app/core/services/my-work.service';
 
 @Component({
@@ -22,47 +9,62 @@ import { CarouselImg } from '@app/core/services/my-work.service';
   imports: [CommonModule],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss',
+  encapsulation: ViewEncapsulation.Emulated,
+
   animations: [
     trigger('carouselAnimation', [
-      state('void', style({ opacity: 0.5 })),
-      state('*', style({ opacity: 1 })),
-      transition(':enter', [animate('1s ease-in')]),
-      transition(':leave', [animate('1s ease-out')]),
+      transition('void => *', [
+        style({
+          transform: 'translateX({{start}}20%)',
+          opacity: 0,
+          visibility: 'hidden',
+        }),
+        animate(
+          '0.3s 0.3s ease-out',
+          style({
+            opacity: 1,
+            visibility: 'visible',
+            transform: 'translateX(0)',
+          }),
+        ),
+      ]),
+      transition('* => void', [
+        style({
+          opacity: 0,
+          visibility: 'hidden',
+          transform: 'translateX(0)',
+        }),
+        animate(
+          '0.3s ease-out',
+          style({ transform: 'translateX({{end}}20%)' }),
+        ),
+      ]),
     ]),
   ],
 })
-export class CarouselComponent implements AfterViewInit {
+export class CarouselComponent {
   @Input() carouselImgs: CarouselImg[] = [];
 
-  @ViewChild('imageElement') imageElement!: ElementRef<HTMLImageElement>;
-  @ViewChild('imageElWrapper') imageElWrapper!: ElementRef<HTMLImageElement>;
-
   currentSlide = 0;
-  contentHeight = 0;
 
-  ngAfterViewInit() {
-    setTimeout(() => {
-      this.updateContentHeight();
-    }, 500);
-  }
+  lastDirection: 'left' | 'right' | 'none' = 'none';
 
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.updateContentHeight();
-  }
-
-  updateContentHeight() {
-    this.contentHeight =
-      this.imageElement.nativeElement.getBoundingClientRect().height;
-  }
-
-  onPrevious() {
-    const previous = this.currentSlide - 1;
-    this.currentSlide = previous < 0 ? this.carouselImgs.length - 1 : previous;
-  }
-
-  onNext() {
-    const next = this.currentSlide + 1;
-    this.currentSlide = next === this.carouselImgs.length ? 0 : next;
+  public onNavigate(dir: 'left' | 'right'): void {
+    this.lastDirection = dir;
+    switch (dir) {
+      case 'left':
+        if (this.currentSlide === 0) {
+          this.currentSlide = this.carouselImgs.length - 1;
+        } else {
+          this.currentSlide = this.currentSlide - 1;
+        }
+        break;
+      case 'right':
+        if (this.currentSlide === this.carouselImgs.length - 1) {
+          this.currentSlide = 0;
+        } else {
+          this.currentSlide = this.currentSlide + 1;
+        }
+    }
   }
 }
